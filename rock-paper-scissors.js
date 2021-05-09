240104909d221d93524788d1cf926945a28d23e8
fdd <fddfddfddfddfddfdd@gmail.com>
2021-05-09 16:02:51 +0300

    Finished and fixed js file. Haven't found any more bugs

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,79 +1,81 @@
-
-let playerScore = 0;
-let computerScore = 0;
+let playerScore = 0
+let computerScore = 0
 
 for (i=0; i<5; i++){
-    do{
-        const computerSelection = computerPlay();
-        const playerSelection = playerPlay();
-    } while (computerSelection == playerSelection)
-    
-    
-    result = playRound(playerSelection, computerSelection);
-    console.log(result);
-    if (result.includes('win')){
-        playerScore += 1;
-    } else {
-        computerScore += 1;
+    let computerSelection = computerPlay()
+    let playerSelection = playerPlay()
+
+    while (computerSelection == playerSelection){
+        let computerSelection = computerPlay()
+        let playerSelection = playerPlay()
     }
-    console.log(`Score:   Player = ${playerScore}  |  Computer = ${computerScore}`);
+
+    result = playRound(playerSelection, computerSelection)
+
+    if (result.includes('win')){
+        playerScore += 1
+    } else {
+        computerScore += 1
+    }
+
+    console.log(`Score:   Player = ${playerScore}  |  Computer = ${computerScore}   |   ${result}`)
 }
 
 function computerPlay()
 {
     const max = 3
-    let selec = Math.floor(Math.random() * max) + 1;
-    if (selec == 1) return "Rock";
-    if (selec == 2) return "Paper";
-    if (selec == 3) return "Scissors";
+    let selec = Math.floor(Math.random() * max) + 1
+    let choice
+    if (selec == 1) choice = "Rock"
+    if (selec == 2) choice = "Paper"
+    if (selec == 3) choice = "Scissors"
+    return choice;
 }
 
 function playerPlay()
 {
-    let valid = "no pass";
-    let selection;
-    while(valid != "pass"){
-        selection = prompt("What will you play? (Enter rock, paper or scissors)");
+    let hold = true
+    let selection
+    while(hold){
+        selection = prompt("What will you play? (Enter rock, paper or scissors)")
         if (typeof selection == 'string' && selection.toLowerCase() == 'rock' || selection.toLowerCase() == 'paper' || selection.toLowerCase() == 'scissors'){
-            valid = "pass";
+            hold = false
         } else{
-            console.log('Try again and make sure you follow the instructions');
+            console.log('Try again and make sure you follow the instructions')
         }
     }
-    return selection.charAt(0).toUpperCase() + selection.slice(1).toLowerCase();
+    return selection.charAt(0).toUpperCase() + selection.slice(1).toLowerCase()
 }
 
-function playRound(playerSelection, computerSelection)
-{
-    for (i=0; i<5; i++){
-        if (playerSelection == 'Rock'){
-            if (computerSelection == 'Paper'){
-                let result = `You lose. ${computerSelection} beats ${playerSelection}`;
-                return result;
-            }else{
-                let result = `You win. ${playerSelection} beats ${computerSelection}`;
-                return result;
-            }
-        }
+function playRound(playerSelection, computerSelection){
 
-        if (playerSelection == 'Paper'){
-            if (computerSelection == 'Scissors'){
-                let result = `You lose. ${computerSelection} beats ${playerSelection}`;
-                return result;
-            }else{
-                let result = `You win. ${playerSelection} beats ${computerSelection}`;
-                return result;
-            }
+    if (playerSelection == 'Rock'){
+        if (computerSelection == 'Paper'){
+            let result = `You lose. ${computerSelection} beats ${playerSelection}`
+            return result
+        }else{
+            let result = `You win. ${playerSelection} beats ${computerSelection}`
+            return result
         }
+    }
 
-        if (playerSelection == 'Scissors'){
-            if (computerSelection == 'Rock'){
-                let result = `You lose. ${computerSelection} beats ${playerSelection}`;
-                return result;
-            }else{
-                let result = `You win. ${playerSelection} beats ${computerSelection}`;
-                return result;
-            }
+    if (playerSelection == 'Paper'){
+        if (computerSelection == 'Scissors'){
+            let result = `You lose. ${computerSelection} beats ${playerSelection}`
+            return result
+        }else{
+            let result = `You win. ${playerSelection} beats ${computerSelection}`
+            return result
+        }
+    }
+
+    if (playerSelection == 'Scissors'){
+        if (computerSelection == 'Rock'){
+            let result = `You lose. ${computerSelection} beats ${playerSelection}`
+            return result
+        }else{
+            let result = `You win. ${playerSelection} beats ${computerSelection}`
+            return result
         }
     }
 }
